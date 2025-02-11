37f8bc9fd149ae6562d034e4c755bc5fa8073420
cxxpertaylor <cooper.b.taylor@gmail.com>
2025-02-10 22:23:46 -0700

    Add project files.

---
+++ Mission06_Taylor/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission06_Taylor.Models;
+
+namespace Mission06_Taylor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext _context; //naming convention to say that this is a private variable brought in through the constructor
+        public HomeController(MovieContext temp) //Constructor. Builds an instance of the database, brings it in and stores it in a permanent variable.
+        { 
+            _context = temp;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnowJoel()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(Movie response)
+        {
+            _context.Movies.Add(response); //Adds that record from the response to the database
+            _context.SaveChanges(); //Commits to the database
+
+            return View("SubmissionSuccessful", response);
+        }
+    }
+}
---
+++ Mission06_Taylor/Models/MovieContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_Taylor.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base (options) //Constructor 
+        { 
+        
+        }
+        public DbSet<Movie> Movies { get; set; } //Create the table in the database
+    }
+}
